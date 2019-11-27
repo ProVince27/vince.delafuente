7a4af75543f6db7f132581add1b0d9a19d5c0dc5
Vincent Dela Fuente <vince.delafuente027@gmail.com>
2019-11-27 11:32:17 +0800

    update adminlte3 version

--- resources/app/dashboard/dashboard-admin/dashboard-admin.blade.php
+++ resources/app/dashboard/dashboard-admin/dashboard-admin.blade.php
@@ -18,11 +18,22 @@
                     <card-body>
                         <p>Tis is a content</p>
                         <bs-button>Click</bs-button>
+                            <ul>
+                                <li v-for="(ssr,index) in sortable" :key="ssr.id" v-html="ssr.name"></li>
+                            </ul>
+                        
+                        <div>
+                            
+                            {{-- <draggable v-model="sortable" >
+                                <div style="height:30px;border:1px solid red;" class="m-10" v-for="srt in sortable" :key="srt.id" v-html="srt.name"></div>
+                            </draggable> --}}
+                            
+                        </div>
                     </card-body>
-                    <card-footer>This is footer</card-footer>
+                    <card-footer class="mx-center">This is footer</card-footer>
                 </card>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-3 p-10">
                 <info-box>
                     <info-box-icon class="bg-info elevation-1" icon="fas fa-cog" />
                     <info-box-content>
--- resources/app/dashboard/dashboard-admin/dashboard-admin.vue
+++ resources/app/dashboard/dashboard-admin/dashboard-admin.vue
@@ -3,15 +3,39 @@ import { addComponents } from 'utils/bundle'
 // import {CardHeader,Card,CardBody} from 'components/containers/cards'
 import * as contents from 'components/containers'
 import * as buttons from 'components/button'
+import draggable from 'vuedraggable'
 
 export default {
     name:'dashboard-admin',
     template:'#dashboard-admin',
     components:{
         ...contents,
-        ...buttons
+        ...buttons,
+        ...addComponents(draggable)
         // addComponents(...contents),
         // addComponents(...buttons)
     },
+    data(){
+        return {
+            sortable:[
+                {
+                    id:1,
+                    name:"sample1"
+                },
+                {
+                    id:2,
+                    name:"sample2"
+                },
+                {
+                    id:3,
+                    name:"sample3"
+                }
+
+            ]
+        }
+    },
+    mounted(){
+        console.log(this.sortable)
+    }
 }
 </script>
